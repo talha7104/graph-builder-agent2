d55e6d1ddc27c550955e83a3d57ef2eda3aef692
Ngoc Le Dinh <49720223+dinhngoc267@users.noreply.github.com>
2025-05-30 23:50:21 +0700

    Update distill.py

--- src/distill.py
+++ src/distill.py
@@ -7,12 +7,8 @@ from src.nodes import SectionDistillerNode, DocParserNode
 from src.models import MyUsage
 from src.nodes.single_distiller import SingleDistillerNode
 
-# sample = "...Your sample"
-data = json.load(open("/home/ju/PycharmProjects/docgraph-construction/notebooks/history_textbook.json"))
-sample = "Tình hình chính trị, kinh tế, văn hóa dưới triều Nguyễn (Nửa đầu thế kỉ XIX)"
+sample = "...Your sample"
 
-for i,(key, value) in enumerate(data['Lớp 10']['Việt Nam ở nửa đầu thế kỉ XIX']["Tình hình chính trị, kinh tế, văn hóa dưới triều Nguyễn (Nửa đầu thế kỉ XIX)"].items()):
-    sample += f"\n{str(i)} {key}\n{value}"
 
 graph = Graph(nodes=[DocParserNode, SectionDistillerNode])
 state=MyUsage()
